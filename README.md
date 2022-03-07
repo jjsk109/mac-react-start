83ee3ea532d6d9895ddc6a46d927fe313a2d664e
jjsk109 <jjsk109@gmail.com>
2022-03-07 23:28:54 +0900

    mac react first step

--- README.md
+++ README.md
@@ -1,70 +1,36 @@
-# Getting Started with Create React App
+### Mac yarn 설치
+# 홈브루를 /opt 디렉토리에 설치하기 위해 이동
+cd /opt
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# 루트 권한으로 homebrew 폴더를 만든다
+sudo mkdir homebrew
 
-## Available Scripts
+# homebrew 폴더의 루트 권한을 유저로 바꿔준다
+sudo chown -R $(whoami) /opt/homebrew
+//sudo chown -R 유저명 경로 = 경로의 권한을 유저한테 준다는 뜻 
 
-In the project directory, you can run:
+# homebrew 다운로드
+curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 
-### `yarn start`
+# homebrew bin 디렉토리를 PATH에 추가
+echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.zshrc
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+# homebrew 홈페이지에 있던 명령어를 이제 실행!
+/bin/bash -c "$(curl -fsSL https://gist.githubusercontent.com/nrubin29/bea5aa83e8dfa91370fe83b62dad6dfa/raw/48f48f7fef21abb308e129a80b3214c2538fc611/homebrew_m1.sh)"
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+# 설치완료 
 
-### `yarn test`
+# 참고사이트
+https://im-designloper.tistory.com/53 -- 감사합니다 --
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-### `yarn build`
+### 리액트를 활요한 기본 퍼블리싱 작업
+# 난이도 : 5점 중 3점
+쉬운건 확실히 html,css, js를 활용한 퍼블리싱 혹은 개발이 쉬울 것이다 
+그러나 
+손에 익어서 작업이 쉬운 것이지 react 라이브러리들을 활용하고 prop으로 넘어가는 것을 잘 활용한다면 훨신 더 쉬울 것 같아 3점을 줬고 더 쉬워졌으면 한다 
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+### 사용 슬라이드
+https://react-slick.neostack.com/docs/example/simple-slider -- 도움이 많이 됬습니다.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Slider from 'react-slick/lib/slider';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import logo from './img/tree-gf79811420_1280.jpg';
 
 function App() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+    
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          header  
+        </div>
+        <div>
+          <ul className='nev_bar'>
+            <li>menu1</li>
+            <li>menu2</li>
+            <li>menu3</li>
+          </ul>
+        </div>
+        
+      </header> 
+      <section className="section1">
+        <img src={logo} className='App-img' alt='React' />
+        <h1>Section1</h1>  
+      </section>
+        <div className='slide_contens'>
+      <Slider {...settings} >
+        <div >
+            <h3>What is Lorem Ipsum?</h3>
+            <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+            
+          </div>
+          <div >
+            <h3>Why do we use it?</h3>
+            <div>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</div>
+          </div>
+          <div >
+            <h3>Where does it come from?</h3>
+            <div>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</div>
+          </div>
+          <div >
+            <h3>Where can I get some?</h3>
+            <div>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</div>
+          </div>
+         
+        </Slider>
+          </div>
+      <section className="section2">
+        <h1>Section2</h1>
+      </section>
+      <section className="section1">
+        <h1>Section3</h1>
+      </section>
+      
+      <footer className='App-footer'>
+        footer
+      </footer>
     </div>
   );
 }
